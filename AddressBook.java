484252bb50e043590f69979449ab590d03d7991d
renu100 <renu.chandraker08@gmail.com>
2021-09-24 10:58:23 +0530

    Welcome

---
+++ AddressBook.java
@@ -0,0 +1,13 @@
+package com.DayAddressBook;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AddressBook {
+	private static final Logger log = LogManager.getLogger(AddressBook.class);
+
+	public static void main(String[] args) {
+		log.info("Welcome");
+	}
+
+}
