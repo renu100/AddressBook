4d0a2bcad626bce1935c27a56a65cd8a29b2c778
renu100 <renu.chandraker08@gmail.com>
2021-10-29 00:53:57 +0530

    Added: Ability to count persons by city or state

--- AddressBookService.java
+++ AddressBookService.java
@@ -193,12 +193,13 @@ public class AddressBookService {
 		public void driver() {
 			while (true) {
 				log.info("Welcome to Address Book");
-				log.info("1 Add\n2 Edit \n3 Delete\n4 Print\n5 Serach Person By City/State\n6 search by persone");
+				log.info(
+						"1 Add\n2 Edit \n3 Delete\n4 Print\n5 Serach Person By City/State\n6 View person by City/State\n7 Person count by City/State");
 				log.info("Enter option");
 				int option = sc.nextInt();
 				sc.nextLine();
 
-				if (option == 7)
+				if (option == 8)
 					break;
 				switch (option) {
 				case 1:
@@ -219,6 +220,9 @@ public class AddressBookService {
 
 				case 6:
 					this.viewByCityOrState();
+					break;
+				case 7:
+					this.countByCityOrState();
 					break;
 				default:
 					log.info("Invalid Choice");
@@ -264,8 +268,7 @@ public class AddressBookService {
 			int option = 0;
 			int EXIT_VALUE = 3;
 			while (option != EXIT_VALUE) {
-				log.info(
-						"Enter your choice \n1. person details by city \n2. person details by state \nEXIT_VALUE. Exit");
+				log.info("Enter your choice \n1. person details by city \n2. person details by state \n3.Exit");
 				option = sc.nextInt();
 				switch (option) {
 				case 1:
@@ -287,18 +290,51 @@ public class AddressBookService {
 				case 2:
 					log.info("Enter state name ");
 					String state;
-					Dictionary infoCity1 = new Hashtable();
-					log.info("Enter city name:");
+					Dictionary infoState = new Hashtable();
 					state = sc.next();
 					List<AddressBookModel> streamList1 = addressBookModels.stream()
 							.filter(person1 -> state.equals(person1.getState())).collect(Collectors.toList());
-					log.info("Persons in city: " + streamList1);
+					log.info("Persons in state: " + streamList1);
 					for (AddressBookModel a : addressBookModels) {
 						if (a.state.equals(state)) {
-							infoCity1.put(a.firstName, state);
+							infoState.put(a.firstName, state);
 						}
 					}
-					log.info("The Dictionary Contains:" + infoCity1);
+					log.info("The Dictionary Contains:" + infoState);
+					break;
+				default:
+					break;
+				}
+			}
+
+		}
+
+		/**
+		 * Method to count person by city or state
+		 */
+		public void countByCityOrState() {
+			int option = 0;
+			int EXIT_VALUE = 3;
+			while (option != EXIT_VALUE) {
+				log.info("Enter your choice \n1. count person by city \n2. count person by state \n3.Exit");
+				option = sc.nextInt();
+				switch (option) {
+				case 1:
+					String city;
+					log.info("Enter city name:");
+					city = sc.next();
+					int countCity = (int) addressBookModels.stream().filter(person1 -> city.equals(person1.getCity()))
+							.count();
+					System.out.println("Total Person Count in " + city + " city is:" + countCity);
+					break;
+
+				case 2:
+					String state;
+					log.info("Enter state name ");
+					state = sc.next();
+					int countState = (int) addressBookModels.stream()
+							.filter(person1 -> state.equals(person1.getState())).count();
+					System.out.println("Total Person Count in " + state + " state is:" + countState);
 					break;
 				default:
 					break;
